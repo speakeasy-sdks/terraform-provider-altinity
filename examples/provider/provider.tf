e3763fe1cc54a5039c436034faa153606b2c03c5
Thomas Rooney <thomas@resiliencycollective.com>
2023-07-05 21:51:25 +0100

    add examples in

---
+++ examples/provider/provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    altinity = {
+      source  = "altinity/altinity"
+      version = "0.0.1"
+    }
+  }
+}
+
+provider "altinity" {
+  # Configuration options
+}
